7cf38f8d80615fa95128ac5794f672c5d81c6e61
Abhinav reddy Boddu <abhinavreddyboddu@gmail.com>
2023-01-29 22:02:00 +0530

    for Netlify

--- src/utils/Communicate.js
+++ src/utils/Communicate.js
@@ -1,7 +1,6 @@
 import axios from "axios"
-require('dotenv').config()
 
-const BaseUrl = process.env.ServerUrl || 'http://localhost:5000'
+const BaseUrl = process.env.NODE_ENV==='development' ? 'http://localhost:5000' : 'https://reddit-odhk.onrender.com'
 
 let token = window.localStorage.getItem('Greddit:token') || null
 
