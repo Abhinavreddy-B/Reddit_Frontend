13c02b9e733ef26fd2de951ccdef2e6825381547
Abhinav reddy Boddu <abhinavreddyboddu@gmail.com>
2023-01-29 21:34:55 +0530

    Remove proxy, instead using url through config file

--- src/utils/Communicate.js
+++ src/utils/Communicate.js
@@ -1,4 +1,7 @@
 import axios from "axios"
+require('dotenv').config()
+
+const BaseUrl = process.env.ServerUrl || 'http://localhost:5000'
 
 let token = window.localStorage.getItem('Greddit:token') || null
 
@@ -22,32 +25,32 @@ const setToken = (user) => {
 }
 
 const signIn = async (credentials, type) => {
-    const res = await axios.post(`/api/users/login`, credentials)
+    const res = await axios.post(`${BaseUrl}/api/users/login`, credentials)
     return res.data
 }
 
 const signUp = async (credentials, type) => {
-    const res = await axios.post(`/api/users/signup`, credentials)
+    const res = await axios.post(`${BaseUrl}/api/users/signup`, credentials)
     return res.data
 }
 
 const GetUserData = async () => {
-    const res = await axios.get(`/api/users/`, config())
+    const res = await axios.get(`${BaseUrl}/api/users/`, config())
     return res.data
 }
 
 const UpdateUserData = async (newData) => {
-    const res = await axios.put(`/api/users/`, newData, config())
+    const res = await axios.put(`${BaseUrl}/api/users/`, newData, config())
     return res.data
 }
 
 const getFollowers = async () => {
-    const res = await axios.get(`/api/users/followers`, config())
+    const res = await axios.get(`${BaseUrl}/api/users/followers`, config())
     return res.data
 }
 
 const getFollowing = async () => {
-    const res = await axios.get(`/api/users/following`, config())
+    const res = await axios.get(`${BaseUrl}/api/users/following`, config())
     return res.data
 }
 
@@ -55,7 +58,7 @@ const DeleteFollower = async (id) => {
     const data = {
         FollowerId: id
     }
-    const res = await axios.post(`/api/users/followers/remove`, data, config())
+    const res = await axios.post(`${BaseUrl}/api/users/followers/remove`, data, config())
     return res
 }
 
@@ -63,52 +66,52 @@ const DeleteFollowing = async (id) => {
     const data = {
         FollowingId: id
     }
-    const res = await axios.post(`/api/users/following/remove`, data, config())
+    const res = await axios.post(`${BaseUrl}/api/users/following/remove`, data, config())
     return res
 }
 
 const AddSubGreddit = async (data) => {
-    const res = await axios.post(`/api/subgreddit`, data, config())
+    const res = await axios.post(`${BaseUrl}/api/subgreddit`, data, config())
     return res.data
 }
 
 const GetOwnedSubGreddits = async () => {
-    const res = await axios.get(`/api/subgreddit`, config())
+    const res = await axios.get(`${BaseUrl}/api/subgreddit`, config())
     return res.data
 }
 
 const DeleteSubGreddit = async (id) => {
-    const res = await axios.delete(`/api/subgreddit/${id}`, config())
+    const res = await axios.delete(`${BaseUrl}/api/subgreddit/${id}`, config())
     return res
 }
 
 const GetAllSubGreddits = async () => {
-    const res = await axios.get(`/api/subgreddit/all`, config())
+    const res = await axios.get(`${BaseUrl}/api/subgreddit/all`, config())
     return res.data
 }
 
 const GetJoinedSubGreddits = async () => {
-    const res = await axios.get(`/api/users/subgreddits`, config())
+    const res = await axios.get(`${BaseUrl}/api/users/subgreddits`, config())
     return res.data
 }
 
 const LeaveSubGreddit = async (id) => {
-    const res = await axios.delete(`/api/subgreddit/leave/${id}`, config())
+    const res = await axios.delete(`${BaseUrl}/api/subgreddit/leave/${id}`, config())
     return res
 }
 
 const GetSingleSubGredditPage = async (id) => {
-    const res = await axios.get(`/api/subgreddit/${id}`, config())
+    const res = await axios.get(`${BaseUrl}/api/subgreddit/${id}`, config())
     return res.data
 }
 
 const PostUpvote = async (id) => {
-    const res = await axios.get(`/api/post/${id}/upvote`, config())
+    const res = await axios.get(`${BaseUrl}/api/post/${id}/upvote`, config())
     return res
 }
 
 const PostDownvote = async (id) => {
-    const res = await axios.get(`/api/post/${id}/downvote`, config())
+    const res = await axios.get(`${BaseUrl}/api/post/${id}/downvote`, config())
     return res
 }
 
@@ -116,7 +119,7 @@ const PostComment = async (id, comment) => {
     const data = {
         comment
     }
-    const res = await axios.post(`/api/post/comment/${id}`, data, config())
+    const res = await axios.post(`${BaseUrl}/api/post/comment/${id}`, data, config())
     return res.data
 }
 
@@ -124,27 +127,27 @@ const AddPost = async (id, Text) => {
     const data = {
         Text
     }
-    const res = await axios.post(`/api/post/${id}`, data, config())
+    const res = await axios.post(`${BaseUrl}/api/post/${id}`, data, config())
     return res.data
 }
 
 const SavePost = async (id) => {
-    const res = await axios.get(`/api/post/${id}/save`, config())
+    const res = await axios.get(`${BaseUrl}/api/post/${id}/save`, config())
     return res
 }
 
 const FollowPostOwner = async (id) => {
-    const res = await axios.get(`/api/post/${id}/followowner`, config())
+    const res = await axios.get(`${BaseUrl}/api/post/${id}/followowner`, config())
     return res
 }
 
 const SendJoinRequest = async (id) => {
-    const res = await axios.get(`/api/subgreddit/${id}/join`, config())
+    const res = await axios.get(`${BaseUrl}/api/subgreddit/${id}/join`, config())
     return res.data
 }
 
 const GetJoinRequests = async (id) => {
-    const res = await axios.get(`/api/subgreddit/${id}/requests`, config())
+    const res = await axios.get(`${BaseUrl}/api/subgreddit/${id}/requests`, config())
     return res.data
 }
 
@@ -154,7 +157,7 @@ const AcceptJoinRequests = async (SubGredditId, userId) => {
         userId
     }
 
-    const res = await axios.post(`/api/subgreddit/accept`, data, config())
+    const res = await axios.post(`${BaseUrl}/api/subgreddit/accept`, data, config())
     return res.data
 }
 
@@ -164,27 +167,27 @@ const RejectJoinRequests = async (SubGredditId, userId) => {
         userId
     }
 
-    const res = await axios.post(`/api/subgreddit/reject`, data, config())
+    const res = await axios.post(`${BaseUrl}/api/subgreddit/reject`, data, config())
     return res.data
 }
 
 const GetSubGredditUsers = async (id) => {
-    const res = await axios.get(`/api/subgreddit/${id}/users`, config())
+    const res = await axios.get(`${BaseUrl}/api/subgreddit/${id}/users`, config())
     return res.data
 }
 
 const GetSavedPosts = async () => {
-    const res = await axios.get(`/api/users/savedposts`, config())
+    const res = await axios.get(`${BaseUrl}/api/users/savedposts`, config())
     return res.data
 }
 
 const RemoveSavedPost = async (id) => {
-    const res = await axios.delete(`/api/users/savedposts/${id}`, config())
+    const res = await axios.delete(`${BaseUrl}/api/users/savedposts/${id}`, config())
     return res
 }
 
 const GetReports = async (id) => {
-    const res = await axios.get(`/api/subgreddit/${id}/reports`, config())
+    const res = await axios.get(`${BaseUrl}/api/subgreddit/${id}/reports`, config())
     return res.data
 }
 
@@ -193,22 +196,22 @@ const PostReport = async (Concern, PostId) => {
         Concern,
         PostId
     }
-    const res = await axios.post(`/api/report`,data, config())
+    const res = await axios.post(`${BaseUrl}/api/report`,data, config())
     return res.data
 }
 
 const IgnoreReport = async (id) => {
-    const res = await axios.get(`/api/report/${id}/ignore`, config())
+    const res = await axios.get(`${BaseUrl}/api/report/${id}/ignore`, config())
     return res
 }
 
 const BlockReport = async (id) => {
-    const res = await axios.get(`/api/report/${id}/block`, config())
+    const res = await axios.get(`${BaseUrl}/api/report/${id}/block`, config())
     return res
 }
 
 const DeleteReport = async (id) => {
-    const res = await axios.get(`/api/report/${id}/delete`, config())
+    const res = await axios.get(`${BaseUrl}/api/report/${id}/delete`, config())
     return res
 }
 
